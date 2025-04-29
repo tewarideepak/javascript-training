93a9204d09603dd1b82db56555f5c3cdf246314a
Deepak Tewari <tewarid69@gmail.com>
2025-04-29 22:41:15 +0530

    Conditional Statements

---
+++ 2. Control Flow/01. Conditional Statements/app.js
@@ -0,0 +1,10 @@
+let a = 10;
+let b = 20;
+
+if (a > b) {
+  console.log("a > b");
+} else if (a < b) {
+  console.log("a < b");
+} else {
+  console.log("a == b");
+}
--- notes.md
+++ notes.md
@@ -70,3 +70,5 @@ console.log(user);
 # Strings
 
 # Type Conversion
+
+# Conditional Statements
